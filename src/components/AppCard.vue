15d703101a1538ebc0421ccf59023a84f39f776a
yelo-o <flyordig@gmail.com>
2023-11-29 22:01:00 +0900

    css module

--- src/components/AppCard.vue
+++ src/components/AppCard.vue
@@ -2,7 +2,8 @@
 	<div class="card">
 		<div class="card-body">
 			<h5 class="card-title red">Card title</h5>
-			<p class="card-text">
+			{{ classes }}
+			<p class="card-text" >
 				Some quick example text to build on the card title and make up the bulk
 				of the card's content.
 			</p>
@@ -21,8 +22,14 @@ export default {
 };
 </script>
 
-<style scoped>
+<style module="classes">
 .red {
 	color: red !important;
 }
 </style>
+
+<!-- <style scoped>
+.red {
+	color: red !important;
+}
+</style> -->

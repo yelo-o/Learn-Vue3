b1787ebbb0b9c623db0c965e3bfc1952cc77431a
yelo-o <flyordig@gmail.com>
2023-11-30 00:12:37 +0900

    one-way data flow

--- src/App.vue
+++ src/App.vue
@@ -9,10 +9,12 @@
 <script>
 import TheNav from './components/TheNav.vue';
 import TheView from './components/TheView.vue';
+import AppCard from './components/AppCard.vue';
 export default {
 	components: {
 		TheNav,
 		TheView,
+		AppCard,
 	},
 	setup() {
 		return {};
--- src/components/TheView.vue
+++ src/components/TheView.vue
@@ -2,16 +2,15 @@
 	<main>
 		<div class="container py-4">
 			<div class="row g-3">
-				<div class="col col-4">
-					<AppCard title="제목1" contents="내용1"></AppCard>
+				<div v-for="post in posts" :key="post.id" class="col col-4">
+					<AppCard
+						:title="post.title"
+						:contents="post.contents"
+						:type="post.type"
+						:is-like="post.isLike"
+						@toggle-like="post.isLike = !post.isLike"
+					></AppCard>
 				</div>
-				<div class="col col-4">
-					<AppCard :title="post.title" :contents="post.contents"></AppCard>
-				</div>
-				<div class="col col-4"><AppCard></AppCard></div>
-				<div class="col col-4"><AppCard></AppCard></div>
-				<div class="col col-4"><AppCard></AppCard></div>
-				<div class="col col-4"><AppCard></AppCard></div>
 			</div>
 		</div>
 	</main>
@@ -25,11 +24,30 @@ export default {
 		AppCard,
 	},
 	setup() {
-		const post = reactive({
+		const obj = reactive({
 			title: '제목2',
 			contents: '내용2',
 		});
-		return { post };
+		const posts = reactive([
+			{ id: 1, title: '제목1', contents: '내용1', isLike: true, type: 'news' },
+			{ id: 2, title: '제목2', contents: '내용2', isLike: true, type: 'news' },
+			{ id: 3, title: '제목3', contents: '내용3', isLike: true, type: 'news' },
+			{
+				id: 4,
+				title: '제목4',
+				contents: '내용4',
+				isLike: false,
+				type: 'notice',
+			},
+			{
+				id: 5,
+				title: '제목5',
+				contents: '내용5',
+				isLike: false,
+				type: 'notice',
+			},
+		]);
+		return { obj, posts };
 	},
 };
 </script>

54f2269845ed38eac9be60d96d354834fbc0328f
yelo-o <flyordig@gmail.com>
2023-11-29 18:08:20 +0900

    divide components

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -13,7 +13,7 @@ module.exports = {
 	},
 	rules: {
 		'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-		'no-unused-vars':'off',
+		'no-unused-vars': 'off',
 		'prettier/prettier': [
 			'error',
 			{
--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { createApp } from 'vue';
 import App from './App.vue';
+// import AppCard from '@/components/AppCard.vue';
 
-createApp(App).mount('#app');
+const app = createApp(App);
+
+// app.component('AppCard', AppCard); //전역등록
+
+app.mount('#app');
 
 import 'bootstrap/dist/js/bootstrap.js';

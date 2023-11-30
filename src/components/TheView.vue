c5c8aaddc39cf3e1641dfde561d02a5322e09b02
yelo-o <flyordig@gmail.com>
2023-11-30 20:40:41 +0900

    custom v-model

--- src/components/TheView.vue
+++ src/components/TheView.vue
@@ -2,7 +2,9 @@
 	<main>
 		<div class="container py-4">
 			<PostCreate @create-post="createPost"></PostCreate>
+
 			<hr class="my-4" />
+
 			<div class="row g-3">
 				<div v-for="post in posts" :key="post.id" class="col col-4">
 					<AppCard
@@ -14,6 +16,21 @@
 					></AppCard>
 				</div>
 			</div>
+
+			<hr class="my-4" />
+			<!-- 
+				modelValue
+				update:modelValue
+			 -->
+
+			<!-- :model-value="username"
+				@update:model-value="value => (username = value)" -->
+			<LabelInput v-model="username" label="이름"></LabelInput>
+			<LabelTitle v-model:title="username" label="제목"></LabelTitle>
+			<Username
+				v-model:firstname="firstname"
+				v-model:lastname="lastname"
+			></Username>
 		</div>
 	</main>
 </template>
@@ -21,11 +38,17 @@
 <script>
 import AppCard from '@/components/AppCard.vue';
 import PostCreate from '@/components/PostCreate.vue';
-import { reactive } from 'vue';
+import LabelInput from '@/components/LabelInput.vue';
+import LabelTitle from '@/components/LabelTitle.vue';
+import Username from '@/components/Username.vue';
+import { reactive, ref } from 'vue';
 export default {
 	components: {
 		AppCard,
 		PostCreate,
+		LabelInput,
+		LabelTitle,
+		Username,
 	},
 	setup() {
 		const obj = reactive({
@@ -57,7 +80,11 @@ export default {
 			posts.push(newPost);
 		};
 
-		return { obj, posts, createPost };
+		const username = ref('');
+		const firstname = ref('');
+		const lastname = ref('');
+
+		return { obj, posts, createPost, username, firstname, lastname };
 	},
 };
 </script>
